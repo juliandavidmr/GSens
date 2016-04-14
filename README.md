1f1118c2bb1c363fb651de6391fd8fb716d04952
Julian David Mo Ra <anlijudavid@gmail.com>
2016-04-13 23:12:20 -0500

    Mensaje de alerta para propiedades de un sensor

--- README.md
+++ README.md
@@ -9,11 +9,10 @@ App Android, iOS y WP de monitoreo en *tiempo real* de varios tipos de sensores.
 
 * [node.js] - Backend orientado a eventos I/O
 * [ionic] - Framework para desarrollo de aplicaciones moviles hibridas.
-* [socket.io] - Gestion de procesos en tiempo real
 
 ### Instalación
 
-Debes tener instalado [node.js] (superior a la 5.0) y [ionic] V2 en tu equipo.
+Debes tener instalado [node.js] (superior a la 5.0) y [ionic] v2 en tu equipo.
 
 ```sh
 $ git clone git@github.com:juliandavidmr/GSens.git
--- app/pages/sensores/sensores.js
+++ app/pages/sensores/sensores.js
@@ -1,7 +1,9 @@
 import {
 	Page,
 	NavController,
-  ActionSheet
+	ActionSheet,
+	Clipboard,
+	Alert
 } from 'ionic-angular';
 
 /*
@@ -26,10 +28,12 @@ export class SensoresPage {
 		this.sensores = [{
 			nombre: 'Sensor de humedad',
 			referencia: 'EERRR',
+			descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 			createAt: new Date()
 		}, {
 			nombre: 'Sensor de nivel',
 			referencia: 'SSR',
+			descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 			createAt: new Date()
 		}]
 	}
@@ -59,10 +63,21 @@ export class SensoresPage {
 				role: 'cancel',
 				handler: () => {
 					console.log('Informacion clicked');
+					this.alerta(sensor);
 				}
 			}]
 		});
 		this.nav.present(actionSheet);
+	}
+
+  alerta(sensor) {
+		//Alerta
+		let alert = Alert.create({
+			title: sensor.nombre,
+			subTitle: sensor.descripcion,
+			buttons: ['OK']
+		});
+		this.nav.present(alert);
 	}
 
 	dismiss() {
